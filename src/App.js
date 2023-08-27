6e1588fdaf555f897e9b5f8f82a422ab8c0071fb
Abdul <abdou159159159@gmail.com>
2023-08-27 22:50:25 +0100

    Porfolio Done and some modifications

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Experience from './components/Experience';
 import Home from './components/Home';
 import NavBar from './components/NavBar';
 import Portfolio from './components/Portfolio';
+import Process from './components/Process';
 import SocialLinks from './components/SocialLinks';
 // import './index.css'
 function App() {
   return <div>
     <NavBar />
     <Home />
+    <Process />
     <About />
     <Portfolio />
     <Experience />
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,35 +1,47 @@
 import React from "react";
 import html from "../assets/html.png";
+import python from "../assets/python.png";
+import java from "../assets/java.png";
+import javascript from "../assets/javascript.png";
+import css from "../assets/css.png";
+import tailwind from "../assets/tailwind.png";
+import mongodb from "../assets/mongodb.png";
+import tensorflow from "../assets/tensorflow.png";
+import pandas from "../assets/pandas.png";
+import react from "../assets/react.png";
+import sql from "../assets/sql.png";
+import github from "../assets/github.png";
+
 const Experience = () => {
   const techs = [
     {
       id: 1,
-      src: html,
-      title: "HTML",
-      style: "shadow-orange-500",
+      src: python,
+      title: "Python",
+      style: "shadow-blue-600",
     },
     {
       id: 2,
-      src: html,
-      title: "HTML",
-      style: "shadow-orange-500",
+      src: java,
+      title: "Java",
+      style: "shadow-red-500",
     },
     {
       id: 3,
-      src: html,
-      title: "HTML",
-      style: "shadow-orange-500",
+      src: javascript,
+      title: "Javascript",
+      style: "shadow-yellow-500",
     },
     {
       id: 4,
-      src: html,
-      title: "HTML",
-      style: "shadow-orange-500",
+      src: react,
+      title: "React",
+      style: "shadow-blue-400",
     },
     {
       id: 5,
-      src: html,
-      title: "HTML",
+      src: tensorflow,
+      title: "TensorFlow",
       style: "shadow-orange-500",
     },
     {
@@ -37,6 +49,44 @@ const Experience = () => {
       src: html,
       title: "HTML",
       style: "shadow-orange-500",
+    },
+    {
+      id: 7,
+      src: css,
+      title: "CSS",
+      style: "shadow-blue-400",
+    },
+    {
+      id: 8,
+      src: tailwind,
+      title: "TailWind",
+      style: "shadow-green-200",
+    },
+    {
+      id: 9,
+      src: mongodb,
+      title: "MongoDB",
+      style: "shadow-green-500",
+    },
+
+    {
+      id: 10,
+      src: pandas,
+      title: "Pandas",
+      style: "shadow-white",
+    },
+
+    {
+      id: 11,
+      src: sql,
+      title: "SQL",
+      style: "shadow-blue-400",
+    },
+    {
+      id: 12,
+      src: github,
+      title: "Github",
+      style: "shadow-gray-700",
     },
   ];
 
@@ -52,12 +102,12 @@ const Experience = () => {
           <p className="py-6 ">These are the technologes I've worked with</p>
         </div>
 
-        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-8 text-center py-8 px-12 sm:px-0">
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
               className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
-              <img src={src} alt="html" className="w-20 mx-auto" />
+              <img src={src} alt={title} className="w-20 mx-auto" />
               <p className="mt-4">{title}</p>
             </div>
           ))}
