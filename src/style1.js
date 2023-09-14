a32df28cf502d484988142d468673b39f521d6aa
Abdul <abdou159159159@gmail.com>
2023-09-14 22:34:37 +0100

    changing color

--- src/style1.js
+++ src/style1.js
@@ -1,3 +1,3 @@
-const style1 = "bg-gradient-to-b from-slate-950 to-blue-950 text-white"
+const style1 = "bg-gradient-to-b from-slate-950 to-blue-900 text-white"
 
 export default style1;
--- src/style2.js
+++ src/style2.js
@@ -1,3 +1,3 @@
-const style2 = "bg-gradient-to-b from-blue-950 to-slate-950 text-white"
+const style2 = "bg-gradient-to-b from-blue-900 to-slate-950 text-white"
 
 export default style2;
