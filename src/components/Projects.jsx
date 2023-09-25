d58815756b63c70184e8b1bc33f2d6af5b574d67
Abdul <abdou159159159@gmail.com>
2023-09-25 22:09:57 +0100

    commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,28 +18,34 @@ const Projects = () => {
       id: 2,
       src: chatbot,
       codeLink: "https://github.com/Abduledd/ChatBot",
-      description: "Ok its working",
+      description:
+        "This project showcases a full-stack web application, creating a dynamic and interactive blogging platform. Users can register, log in, and publish blog posts, which are then displayed for all visitors to see. If logged in, users have the additional ability to edit their own posts.",
+      techs: ["ReactJs", "TailWindCSS"],
     },
     {
       id: 3,
       src: movie,
       codeLink: "https://github.com/Abduledd/Movie-Project",
       description: "Ok its working",
+      techs: ["ReactJs", "TailWindCSS"],
     },
     {
       id: 4,
       src: reactWeather,
       description: "Ok its working",
+      techs: ["ReactJs", "TailWindCSS"],
     },
     {
       id: 5,
       src: reactWeather,
       description: "Ok its working",
+      techs: ["ReactJs", "TailWindCSS"],
     },
     {
       id: 6,
       src: reactWeather,
       description: "Ok its working",
+      techs: ["ReactJs", "TailWindCSS"],
     },
   ];
 
@@ -68,9 +74,9 @@ const Projects = () => {
             <div className="flex flex-col justify-between">
               <div className="text-white">
                 <h3 className="font-bold mb-2">{title}</h3>
-                <p className="text-gray-400 text-sm mb-4">{description}</p>
+                <p className="text-gray-400 text-sm ">{description}</p>
               </div>
-              <div className="flex">
+              <div className="flex my-3">
                 {techs.map((tech, index) => (
                   <span
                     key={index}
